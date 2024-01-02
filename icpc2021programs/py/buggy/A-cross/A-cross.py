a1d1193f690ca719b53fd4d133224add9b4e7508
Jorge De Goyeneche <jorgedg611@uc.cl>
2024-01-02 19:36:00 -0300

    Added 27, changed variable to 1000

---
+++ icpc2021programs/py/buggy/A-cross/A-cross.py
@@ -0,0 +1,139 @@
+import sys
+sys.setrecursionlimit(10000)  # Establece un límite más alto
+
+
+def doit(x, y):
+    ch = g[y][x] if (1 <= x <= X and 1 <= y <= Y) else '.'
+    if ch == ' ':
+        return
+    for i in range(N):
+        x2 = x + (wm[i] if ch == '.' else -wm[i])
+        y2 = y + (wn[i] if ch == '.' else -wn[i])
+        
+        if 1 <= x2 <= X and 1 <= y2 <= Y:
+            if g[y2][x2] == ' ':
+                g[y2][x2] = ch
+                doit(x2, y2)
+
+
+while True:
+    try:
+        X, Y, N = map(int, input().split())
+        g = [[' ' for _ in range(X + 1)] for _ in range(Y + 1)]
+        wm = [0] * N
+        wn = [0] * N
+        for i in range(N):
+            line = input().split()
+            wm[i] = int(line[0])
+            wn[i] = int(line[1])
+            B = int(line[2])
+            pos = 3
+            for j in range(B):  
+                x = int(line[pos])
+                pos = pos + 1
+                y = int(line[pos])
+                pos = pos + 1
+                g[y][x] = '#'
+                x2 = x - wm[i]
+                y2 = y - wn[i]
+                if 1 <= x2 <= X and 1 <= y2 <= Y:
+                    g[y2][x2] = '.'
+
+        for y in range(-Y, 2 * Y + 1):
+            for x in range(-X, 2 * X + 1):
+                doit(x, y)
+
+        for y in range(1, Y + 1):
+            for x in range(1, X + 1):
+                print(g[y][x] if (g[y][x] != ' ') else '.', end='')
+            print()
+        print()
+
+        for y in range(1, Y + 1):
+            for x in range(1, X + 1):
+                print(g[y][x] if (g[y][x] != ' ') else '#', end='')
+            print()
+    except EOFError:
+        break
+
+
+
+# ENFOQUE ITERATIVO
+# def doit(x, y):
+#     # Utilizar una cola para manejar las posiciones a procesar
+#     queue = [(x, y)]
+    
+#     while queue:
+#         x, y = queue.pop(0)  # Cambia a queue.pop() para DFS
+#         ch = g[y][x] if (1 <= x <= X and 1 <= y <= Y) else '.'
+        
+#         if ch == ' ':
+#             continue
+
+#         for i in range(N):
+#             x2 = x + (wm[i] if ch == '.' else -wm[i])
+#             y2 = y + (wn[i] if ch == '.' else -wn[i])
+            
+#             if 1 <= x2 <= X and 1 <= y2 <= Y:
+#                 if g[y2][x2] == ' ':
+#                     g[y2][x2] = ch
+#                     queue.append((x2, y2))  # Agregar nuevas posiciones a la cola
+
+
+
+
+
+
+# def doit(x, y):
+#     ch = g[y][x] if (1 <= x <= X and 1 <= y <= Y) else '.'
+#     if ch == ' ':
+#         return
+#     for i in range(N):
+#         x2 = x + (wm[i] if ch == '.' else -wm[i])
+#         y2 = y + (wn[i] if ch == '.' else -wn[i])
+        
+#         if 1 <= x2 <= X and 1 <= y2 <= Y:
+#             if g[y2][x2] == ' ':
+#                 g[y2][x2] = ch
+#                 doit(x2, y2)
+
+
+# while True:
+#     try:
+#         X, Y, N = map(int, input().split())
+#         g = [[' ' for _ in range(X + 1)] for _ in range(Y + 1)]
+#         wm = [0] * N
+#         wn = [0] * N
+#         for i in range(N):
+#             line = input().split()
+#             wm[i] = int(line[0])
+#             wn[i] = int(line[1])
+#             B = int(line[2])
+#             pos = 3
+#             for j in range(B):  
+#                 x = int(line[pos])
+#                 pos = pos + 1
+#                 y = int(line[pos])
+#                 pos = pos + 1
+#                 g[y][x] = '#'
+#                 x2 = x - wm[i]
+#                 y2 = y - wn[i]
+#                 if 1 <= x2 <= X and 1 <= y2 <= Y:
+#                     g[y2][x2] = '.'
+
+#         for y in range(-Y, 2 * Y + 1):
+#             for x in range(-X, 2 * X + 1):
+#                 doit(x, y)
+
+#         for y in range(1, Y + 1):
+#             for x in range(1, X + 1):
+#                 print(g[y][x] if (g[y][x] != ' ') else '.', end='')
+#             print()
+#         print()
+
+#         for y in range(1, Y + 1):
+#             for x in range(1, X + 1):
+#                 print(g[y][x] if (g[y][x] != ' ') else '#', end='')
+#             print()
+#     except EOFError:
+#         break
